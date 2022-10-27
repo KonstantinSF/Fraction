16159b57e81f2209d2917995396f8410d350c5e4
KonstantinSF <kosmos008@gmail.com>
2022-10-27 22:45:48 +0300

    *=done

--- Fraction/Source.cpp
+++ Fraction/Source.cpp
@@ -127,6 +127,15 @@ public:
 		this->to_proper();
 		return *this;
 	}
+	Fraction& operator*=(Fraction other)
+	{
+		this->to_improper();
+		other.to_improper();
+		set_numerator(get_numerator() * other.get_numerator());
+		set_denominator(get_denominator() * other.get_denominator());
+		this->to_proper();
+		return *this;
+	}
 	
 	//			Methods
 	Fraction& to_improper()
