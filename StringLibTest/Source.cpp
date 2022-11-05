3cf53397ea38d3b72bfa37a5f0d46084f5c18b78
KonstantinSF <kosmos008@gmail.com>
2022-11-05 19:16:06 +0300

    in progress

---
+++ StringLibTest/Source.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include "string.h"
+using std::cin; 
+using std::cout; 
+using std::endl; 
+
+using namespace std; 
+
+void main()
+{
+	setlocale(LC_ALL, ""); 
+	String str1 = "Hello"; 
+}
